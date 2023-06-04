aa7c35ee20b51afaef673b6a34e585d80976357a
Markus / Mark <46672778+marksverdhei@users.noreply.github.com>
2023-06-04 17:59:39 +0200

    Bump to 1.1.0 (#12)
    * Change name of package
    
    * Update package lock
    
    * Apply formatting
    
    ---------
    
    Co-authored-by: marksverdhei <>

--- src/background.js
+++ src/background.js
@@ -1,41 +1,43 @@
-const Article = require("newspaperjs").Article
-const LanguageDetect = require('languagedetect');
+const Article = require("newspaperjs").Article;
+const LanguageDetect = require("languagedetect");
 const languageDetector = new LanguageDetect();
 
 const PROMPTS = {
-  "english": [
-      {
-          "role": "system",
-          "content": "You are a tool that generates saved-you-a-click summaries when provided clickbait titles and content",
-      },
-      {
-          "role": "user",
-          "content": "Your task is the following: Given the title and the body of a clickbait article, provide the information that one would desire or wonder about upon reading the title.\n" +
-            "* Be exact, on point and provide your answer as a repsonse to the title, and make your answers brief, preferrably in one sentence.\n" +
-            "  Example: Costco shoppers shifting away from specific item; CFO says it’s indicator of recession | Beef and steaks.\n" + 
-            "* If the article is a list of N items, simply list each item and nothing else.\n" +
-            "  Example: 5 Fast-Food Chains That Have Gone From the Best to the Bottom In a Few Years | Burgerim, Quiznos, Boston Market, Red Barn, Burger Chef\n" +
-            "* If there is not sufficient information in the article body to answer the title, state so in the answer.\n" +
-            "  Example: Why Big Tech execs are swapping the boardroom for the dojo | Article doesn't say\n\n"
-      },
+  english: [
+    {
+      role: "system",
+      content:
+        "You are a tool that generates saved-you-a-click summaries when provided clickbait titles and content",
+    },
+    {
+      role: "user",
+      content:
+        "Your task is the following: Given the title and the body of a clickbait article, provide the information that one would desire or wonder about upon reading the title.\n" +
+        "* Be exact, on point and provide your answer as a repsonse to the title, and make your answers brief, preferrably in one sentence.\n" +
+        "  Example: Costco shoppers shifting away from specific item; CFO says it’s indicator of recession | Beef and steaks.\n" +
+        "* If the article is a list of N items, simply list each item and nothing else.\n" +
+        "  Example: 5 Fast-Food Chains That Have Gone From the Best to the Bottom In a Few Years | Burgerim, Quiznos, Boston Market, Red Barn, Burger Chef\n" +
+        "* If there is not sufficient information in the article body to answer the title, state so in the answer.\n" +
+        "  Example: Why Big Tech execs are swapping the boardroom for the dojo | Article doesn't say\n\n",
+    },
   ],
   // Options to enter other languages here
-}
+};
 
 function getMissingLanguagePrompt(language) {
   return [
     {
-        "role": "system",
-        "content": `You are a tool that generates saved-you-a-click summaries when provided clickbait titles and content written in ${language}`,
+      role: "system",
+      content: `You are a tool that generates saved-you-a-click summaries when provided clickbait titles and content written in ${language}`,
     },
     {
-        "role": "user",
-        "content": "Given the title and the body of a clickbait article, please provide the information that one might desire upon reading the title. Make the answer as brief as you can.\n" +
+      role: "user",
+      content:
+        "Given the title and the body of a clickbait article, please provide the information that one might desire upon reading the title. Make the answer as brief as you can.\n" +
         `If there is no information in the article content that answers the title, provide your answer as 'The article doesn't say' in ${language}. Write your response in ${language}`,
     },
-  ]
+  ];
 }
-
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
@@ -45,41 +47,47 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "saveMeAClick") {
-    console.log("Sending saveMeAClick to content.js")
+    console.log("Sending saveMeAClick to content.js");
     chrome.tabs.sendMessage(tab.id, { url: info.linkUrl });
   }
 });
 
-
 chrome.runtime.onMessage.addListener(async (message, sender) => {
   if (message.type === "startSpinner") {
-    chrome.tabs.sendMessage(sender.tab.id, { startSpinner: true, url: message.url });
+    chrome.tabs.sendMessage(sender.tab.id, {
+      startSpinner: true,
+      url: message.url,
+    });
   }
   if (message.type === "fetchSummary") {
-    console.log("Received fetchSummary from content.js. Sending spinnner")
+    console.log("Received fetchSummary from content.js. Sending spinnner");
     // Send the message to start the spinner
 
     try {
       const summary = await getSummary(message.url);
-      console.log("Summary received, sending to content.js")
-      chrome.tabs.sendMessage(sender.tab.id, { summary: summary, overlayId: message.overlayId });
+      console.log("Summary received, sending to content.js");
+      chrome.tabs.sendMessage(sender.tab.id, {
+        summary: summary,
+        overlayId: message.overlayId,
+      });
     } catch (error) {
-      console.log("There was an error, sending to content.js")
-      console.log(error.name); 
+      console.log("There was an error, sending to content.js");
+      console.log(error.name);
       console.log(error.message);
       console.log(error.stack);
-      chrome.tabs.sendMessage(sender.tab.id, { error: error.message, overlayId: message.overlayId });
+      chrome.tabs.sendMessage(sender.tab.id, {
+        error: error.message,
+        overlayId: message.overlayId,
+      });
     }
   }
 });
 
-
 function getApiKey() {
   return new Promise((resolve, reject) => {
-    chrome.storage.sync.get(['apiKey'], function(result) {
+    chrome.storage.sync.get(["apiKey"], function (result) {
       if (chrome.runtime.lastError) {
         return reject(chrome.runtime.lastError);
       }
@@ -88,27 +96,22 @@ function getApiKey() {
   });
 }
 
-
 function getDataContent(data) {
   if (
-    (
-      (!data)
-      || (!data.choices)
-      || (!data.choices[0])
-      || (!data.choices[0].message)
-      || (!data.choices[0].message.content)
-
-    )
+    !data ||
+    !data.choices ||
+    !data.choices[0] ||
+    !data.choices[0].message ||
+    !data.choices[0].message.content
   ) {
     return "Data missing";
   } else {
-    return data.choices[0].message.content
+    return data.choices[0].message.content;
   }
-
 }
 
 /**
- * 
+ *
  * @param {str} url url address of the clickbait article or web page
  * @returns {object} summary of the given clickbait content
  */
@@ -116,14 +119,13 @@ async function getSummary(url) {
   const article = await Article(url);
   const title = article.title;
   const body = article.text;
-  
-  if (!(title && body)) 
-    throw new Error("missing article title or text");
-  
+
+  if (!(title && body)) throw new Error("missing article title or text");
+
   const languages = languageDetector.detect(title + body, 1);
-  console.log(languages)
+  console.log(languages);
   const language = languages[0][0];
-  console.log(language)
+  console.log(language);
 
   if (language in PROMPTS) {
     prePrompt = PROMPTS[language];
@@ -132,11 +134,11 @@ async function getSummary(url) {
   }
 
   fullPrompt = prePrompt.concat([
-    {"role": "user", "content": `<article-title>${title}</article-title>`},
-    {"role": "user", "content": `<article-body>${body}</article-body>`},
-  ])
-  console.log(fullPrompt)
-  
+    { role: "user", content: `<article-title>${title}</article-title>` },
+    { role: "user", content: `<article-body>${body}</article-body>` },
+  ]);
+  console.log(fullPrompt);
+
   let result = {
     title: title,
     body: body,
@@ -151,12 +153,12 @@ async function getSummary(url) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${apiKey}`
+        Authorization: `Bearer ${apiKey}`,
       },
       body: JSON.stringify({
-        "model": "gpt-3.5-turbo",
-        "messages": fullPrompt,
-        "temperature": 0.7
+        model: "gpt-3.5-turbo",
+        messages: fullPrompt,
+        temperature: 0.7,
       }),
     });
 
